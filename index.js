60a303e6e1b99337069692e8662b0044379cb2d0
derek <derekle.creative@gmail.com>
2021-08-20 19:30:58 -0500

    commit tree d8c9f824b817aeb335f48ddaaaf2d72a2a5c6fd5

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(y, c, m) {
+    this.year = y;
+    this.color = c;
+    this.model = m;
+}
+
+function Driver(n, a, x) {
+    this.name = n;
+    this.age = a;
+    this.experience = x;
+}
+
+function PickupLocation(a, c) {
+    this.address = a;
+    this.city = c;
+}
